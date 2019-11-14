e7f0c8b89f3f9f183e2638aaf8b265ca33cd2efb
larryback <41230266+larryback@users.noreply.github.com>
2019-11-15 01:11:52 +0900

    11월 15일 새벽

---
+++ chapter_01/handle_exceptions_01.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
+
+try:
+    html = urlopen("https://www.pythonscraping.com/pages/error.html")
+except HTTPError as e:
+    print("e")
+
+    # null을 반환하거나, break 문을 실행하거나, 기타 다른 방법을 사용한다
+
+else:
+    # 프로그램을 계속 실행합니다. except 절에서 return이나 break를 사용했다면
+    # 이 else 절은 필요 없습니다.
